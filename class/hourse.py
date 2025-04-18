88ffdb565a29dfe8817f4bb28cf4bff2120b908e
bibi4224202 <bibi4224202@gmail.com>
2025-04-18 10:48:56 +0800

    Update hourse.py

--- class/hourse.py
+++ class/hourse.py
@@ -14,3 +14,20 @@ class Horse():
 horse=Horse(12,'阿拉伯','公')
 horse.get_descriptive()#得先运行这个函数，才得到self.info，不然会报错
 horse.write_speed(10)
+
+
+class Camel(Horse):
+    def __init__(self,age,category,gender):
+
+        super().__init__(age,category,gender)#super().__init__(没有self,age,category,gender)，否则会报错
+
+
+    def write_speed(self,new_speed):
+        self.new_speed=new_speed
+        addr="在沙漠上奔跑的速度为："
+        print(self.info.replace('马','骆驼')+addr+str(self.new_speed)+"km/h.")
+
+camel=Camel(12,"双峰",'公')
+camel.get_descriptive()
+camel.write_speed(10)
+
